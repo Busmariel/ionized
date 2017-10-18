50a281c1860842f100f8557d1a0f69955cb8044a
Marcos Bustamante <busmar4@hotmail.com>
2017-10-18 12:07:30 -0300

    eclipse push test

--- desktop/src/com/mbust/ionized/desktop/DesktopLauncher.java
+++ desktop/src/com/mbust/ionized/desktop/DesktopLauncher.java
@@ -10,6 +10,7 @@ public class DesktopLauncher {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.width = Config.resolutionWidth;
 		config.height = Config.resolutionHeight;
+		config.samples = 16;
 		new LwjglApplication(new GameClass(), config);
 	}
 }
